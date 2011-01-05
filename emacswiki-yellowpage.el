57f7e7e204e5e0683cc35e6451510b7b25d634d0
lugecy <lugecy@gmail.com>
2011-01-06 04:00:08 +0900

    update.

--- emacswiki-yellowpage.el
+++ emacswiki-yellowpage.el
@@ -640,7 +640,7 @@
 (:name icicles-cmd1 :revision 103 :last-modified 2010-12-26T20:28Z)
 (:name icicles-cmd2 :revision 58 :last-modified 2010-12-26T20:29Z)
 (:name icicles-doc1 :revision 258 :last-modified 2011-01-05T17:56Z)
-(:name icicles-doc2 :revision 347 :last-modified 2010-12-26T20:31Z)
+(:name icicles-doc2 :revision 348 :last-modified 2011-01-05T18:06Z)
 (:name icicles-face :revision 62 :last-modified 2010-12-26T20:32Z)
 (:name icicles-fn :revision 520 :last-modified 2011-01-05T17:30Z)
 (:name icicles-install :revision 17 :last-modified 2010-10-11T20:52Z)
