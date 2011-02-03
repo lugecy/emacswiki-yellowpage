90731793f62c34dc118727dc6dd3e6fdaa01034d
lugecy <lugecy@gmail.com>
2011-02-03 09:01:18 +0900

    update.

--- emacswiki-yellowpage.el
+++ emacswiki-yellowpage.el
@@ -1319,7 +1319,7 @@
 (:name sudo-ext :revision 4 :last-modified 2011-01-17T07:10Z)
 (:name sudo-save :revision 2 :last-modified 2008-12-03T21:37Z)
 (:name summarye :revision 2 :last-modified 2010-08-03T13:18Z)
-(:name sunrise-commander :revision 99 :last-modified 2011-01-23T23:45Z)
+(:name sunrise-commander :revision 100 :last-modified 2011-02-02T23:26Z)
 (:name sunrise-x-buttons :revision 10 :last-modified 2010-08-19T20:39Z)
 (:name sunrise-x-checkpoints :revision 8 :last-modified 2010-12-22T22:04Z)
 (:name sunrise-x-loop :revision 10 :last-modified 2010-08-19T20:39Z)
