3ed89d069bc6f6cbb68903829f61b3744b539570
lugecy <lugecy@gmail.com>
2011-03-23 02:01:02 +0900

    update.

--- emacswiki-yellowpage.el
+++ emacswiki-yellowpage.el
@@ -188,7 +188,7 @@
 (:name bpath :revision 3 :last-modified 2009-04-14T06:23Z)
 (:name bracketphobia :revision 5 :last-modified 2008-09-05T05:53Z)
 (:name browse-kill-ring :revision 7 :last-modified 2009-01-20T05:01Z)
-(:name browse-kill-ring+ :revision 17 :last-modified 2011-03-21T04:04Z)
+(:name browse-kill-ring+ :revision 19 :last-modified 2011-03-22T16:38Z)
 (:name browse-urn :revision 4 :last-modified 2008-09-05T05:54Z)
 (:name bst-mode :revision 6 :last-modified 2008-09-05T05:54Z)
 (:name btjunkie-search :revision 1 :last-modified 2009-10-18T19:33Z)
