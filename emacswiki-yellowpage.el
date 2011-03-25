acf65310a8f75da7f60f998d865e423e1bf39eb4
lugecy <lugecy@gmail.com>
2011-03-25 09:01:01 +0900

    update.

--- emacswiki-yellowpage.el
+++ emacswiki-yellowpage.el
@@ -58,7 +58,7 @@
 (:name ansys-mod :revision 6 :last-modified 2008-09-05T05:55Z)
 (:name ant :revision 8 :last-modified 2010-08-03T10:31Z)
 (:name any-ini-mode :revision 6 :last-modified 2008-09-05T05:54Z)
-(:name anything :revision 235 :last-modified 2011-03-04T09:01Z)
+(:name anything :revision 236 :last-modified 2011-03-24T23:19Z)
 (:name anything-R :revision 1 :last-modified 2010-05-21T11:47Z)
 (:name anything-TamasPatrovics :revision 3 :last-modified 2008-09-05T05:53Z)
 (:name anything-ack :revision 2 :last-modified 2010-12-20T04:08Z)
