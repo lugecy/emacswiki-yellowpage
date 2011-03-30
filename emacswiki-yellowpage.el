4a7d49032a939c2dbc3de1004fdf3f45b5cd3e64
lugecy <lugecy@gmail.com>
2011-03-31 02:01:00 +0900

    update.

--- emacswiki-yellowpage.el
+++ emacswiki-yellowpage.el
@@ -632,6 +632,7 @@
 (:name highlight-cl :revision 3 :last-modified 2009-10-12T17:30Z)
 (:name highlight-current-line :revision 4 :last-modified 2005-10-13T17:56Z)
 (:name highline :revision 19 :last-modified 2008-12-20T03:13Z)
+(:name himark :revision 1 :last-modified 2011-03-30T16:05Z)
 (:name himarks-mode :revision 4 :last-modified 2005-10-13T17:56Z)
 (:name historyf :revision 1 :last-modified 2010-07-09T08:38Z)
 (:name hl-line+ :revision 22 :last-modified 2011-02-24T23:23Z)
