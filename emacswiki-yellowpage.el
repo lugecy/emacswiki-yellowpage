0d14abf52c1bbe26ef49b4e7e4b8101924932c18
lugecy <lugecy@gmail.com>
2011-04-12 16:01:01 +0900

    update.

--- emacswiki-yellowpage.el
+++ emacswiki-yellowpage.el
@@ -127,7 +127,7 @@
 (:name auto-complete-verilog :revision 3 :last-modified 2009-04-26T08:52Z)
 (:name auto-document :revision 10 :last-modified 2010-05-04T09:00Z)
 (:name auto-indent-mode :revision 59 :last-modified 2011-04-09T04:10Z)
-(:name auto-install :revision 74 :last-modified 2011-01-29T11:12Z)
+(:name auto-install :revision 75 :last-modified 2011-04-12T06:28Z)
 (:name auto-install-batch-list :revision 27 :last-modified 2011-01-14T02:09Z)
 (:name auto-install-extension :revision 1 :last-modified 2009-01-07T12:31Z)
 (:name auto-mark :revision 7 :last-modified 2008-03-13T02:34Z)
