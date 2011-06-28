26ab126897686a2a31e63431b5fe1e16808939ca
lugecy <lugecy@gmail.com>
2011-06-28 17:01:02 +0900

    update.

--- emacswiki-yellowpage.el
+++ emacswiki-yellowpage.el
@@ -773,6 +773,7 @@
 (:name joystick :revision 2 :last-modified 2008-12-03T21:29Z)
 (:name jpl-reformat :revision 1 :last-modified 2008-01-07T15:48Z)
 (:name jswat :revision 16 :last-modified 2006-08-27T15:29Z)
+(:name judge-indent :revision 1 :last-modified 2011-06-28T07:27Z)
 (:name jump :revision 4 :last-modified 2005-10-13T17:56Z)
 (:name jump-dls :revision 1 :last-modified 2009-02-28T21:25Z)
 (:name jump-or-exec :revision 2 :last-modified 2008-09-02T09:52Z)
