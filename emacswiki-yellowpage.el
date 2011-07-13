17da1cab8d5f685d846aab63043da577907cb797
lugecy <lugecy@gmail.com>
2011-07-14 02:01:02 +0900

    update.

--- emacswiki-yellowpage.el
+++ emacswiki-yellowpage.el
@@ -742,7 +742,7 @@
 (:name iproject :revision 1 :last-modified 2009-09-21T09:49Z)
 (:name irc :revision 2 :last-modified 2010-08-03T12:34Z)
 (:name ireplace :revision 3 :last-modified 2010-08-03T12:35Z)
-(:name irfc :revision 37 :last-modified 2010-12-04T00:49Z)
+(:name irfc :revision 38 :last-modified 2011-07-13T16:14Z)
 (:name isearch+ :revision 48 :last-modified 2011-07-08T03:12Z)
 (:name isearch-all :revision 4 :last-modified 2005-10-13T17:56Z)
 (:name isearch-extension :revision 1 :last-modified 2009-01-16T15:09Z)
