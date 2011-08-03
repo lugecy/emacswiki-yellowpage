b5d578fed4ef6a23837440cfb464ca696fc838f3
lugecy <lugecy@gmail.com>
2011-08-04 06:01:03 +0900

    update.

--- emacswiki-yellowpage.el
+++ emacswiki-yellowpage.el
@@ -460,7 +460,7 @@
 (:name erefactor :revision 4 :last-modified 2011-01-25T12:07Z)
 (:name ergo-movement-mode :revision 17 :last-modified 2010-08-03T12:20Z)
 (:name erobot :revision 4 :last-modified 2005-10-13T17:56Z)
-(:name ess-smart-underscore :revision 13 :last-modified 2011-07-25T16:33Z)
+(:name ess-smart-underscore :revision 14 :last-modified 2011-08-03T20:06Z)
 (:name esv :revision 17 :last-modified 2010-08-03T12:22Z)
 (:name etags-extension :revision 4 :last-modified 2009-02-11T06:57Z)
 (:name etags-select :revision 19 :last-modified 2008-12-15T19:23Z)
