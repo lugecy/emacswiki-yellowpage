43f7a23fd7af9d8f9403ccc68183f8df957a06de
lugecy <lugecy@gmail.com>
2011-10-02 02:01:03 +0900

    update.

--- emacswiki-yellowpage.el
+++ emacswiki-yellowpage.el
@@ -687,7 +687,7 @@
 (:name idle-scroll :revision 8 :last-modified 2009-11-21T00:40Z)
 (:name idlehelp :revision 4 :last-modified 2005-10-13T17:56Z)
 (:name idomenu :revision 3 :last-modified 2010-02-10T23:00Z)
-(:name iedit :revision 3 :last-modified 2011-02-25T07:18Z)
+(:name iedit :revision 4 :last-modified 2011-10-01T16:13Z)
 (:name ifind :revision 5 :last-modified 2009-03-09T16:35Z)
 (:name ifind-mode :revision 1 :last-modified 2010-04-06T20:55Z)
 (:name igrep :revision 8 :last-modified 2009-12-24T18:27Z)
