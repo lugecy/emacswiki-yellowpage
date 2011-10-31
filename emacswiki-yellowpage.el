7caa3694859d996b7c394ed65f12e47867a37e24
lugecy <lugecy@gmail.com>
2011-11-01 08:01:26 +0900

    update.

--- emacswiki-yellowpage.el
+++ emacswiki-yellowpage.el
@@ -342,7 +342,7 @@
 (:name diff-mode- :revision 18 :last-modified 2011-02-11T22:42Z)
 (:name diffstat :revision 3 :last-modified 2011-09-05T06:48Z)
 (:name dim-google :revision 2 :last-modified 2009-02-12T08:48Z)
-(:name dired+ :revision 115 :last-modified 2011-10-24T14:24Z)
+(:name dired+ :revision 116 :last-modified 2011-10-31T22:14Z)
 (:name dired-column-widths :revision 6 :last-modified 2009-03-09T17:54Z)
 (:name dired-details :revision 7 :last-modified 2011-02-21T02:48Z)
 (:name dired-details+ :revision 29 :last-modified 2011-02-24T22:59Z)
