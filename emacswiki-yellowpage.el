beaf364cc3e36034370a6372decb18065b920df3
lugecy <lugecy@gmail.com>
2011-12-11 23:01:03 +0900

    update.

--- emacswiki-yellowpage.el
+++ emacswiki-yellowpage.el
@@ -1086,7 +1086,7 @@
 (:name org-jira :revision 3 :last-modified 2009-06-04T15:19Z)
 (:name org-json :revision 6 :last-modified 2011-05-29T12:55Z)
 (:name org-mairix :revision 4 :last-modified 2007-10-01T21:05Z)
-(:name org-occur-goto :revision 2 :last-modified 2011-04-25T19:22Z)
+(:name org-occur-goto :revision 3 :last-modified 2011-12-11T13:06Z)
 (:name org-oddmuse :revision 1 :last-modified 2009-03-09T14:34Z)
 (:name org-outlook :revision 3 :last-modified 2011-04-08T13:51Z)
 (:name org-pua :revision 1 :last-modified 2009-04-11T17:08Z)
